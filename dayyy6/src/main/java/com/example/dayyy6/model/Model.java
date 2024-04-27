7face10731c095034b3c633b73cd1340e18907f7
Amruthavarshini-05 <varshiniamrutha202@gmail.com>
2024-04-27 11:17:27 +0530

    irc project upload

---
+++ dayyy6/src/main/java/com/example/dayyy6/model/Model.java
@@ -0,0 +1,27 @@
+package com.example.dayyy6.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+//identify have all attributes
+public class Model 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private String roles;
+}
